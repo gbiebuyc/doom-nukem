d1960ffeb606481f18867a15f19532e3fdd15057
hibouglorieux <azganor@gmail.com>
2019-06-23 20:42:55 +0200

    fixed leak when new map, reworked detection of current sector for player(maybe adapt it to projectile/monsters needed ?)

--- src/handle_finish.c
+++ src/handle_finish.c
@@ -6,7 +6,7 @@
 /*   By: gbiebuyc <gbiebuyc@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 20:06:00 by gbiebuyc          #+#    #+#             */
-/*   Updated: 2019/06/22 14:50:51 by nallani          ###   ########.fr       */
+/*   Updated: 2019/06/23 20:42:09 by nallani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,11 @@ void	handle_finish(t_data *d)
 	i = -1;
 	while (++i < d->nb_textures)
 		SDL_FreeSurface(d->textures[i]);
+	i = -1;
+	while (++i < MAX_PROJECTILES)
+	{
+		d->projectiles[i].is_active = false;
+	}
 	i = -1;
 	while (i < d->numsectors)
 		clear_sector_sprites(d, i);
