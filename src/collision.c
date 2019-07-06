189e4d90d2079823fce9dc9414b5f6cf36d982b5
hibouglorieux <azganor@gmail.com>
2019-07-06 18:58:57 +0200

    modified proper collision with walls for monster

--- src/collision.c
+++ src/collision.c
@@ -6,7 +6,7 @@
 /*   By: gbiebuyc <gbiebuyc@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/21 21:50:12 by gbiebuyc          #+#    #+#             */
-/*   Updated: 2019/06/28 17:19:08 by gbiebuyc         ###   ########.fr       */
+/*   Updated: 2019/07/06 18:56:19 by nallani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,6 +77,28 @@ bool	collision(t_data *d, t_sector *sect)
 	return (collided + (--recursion ? 0 : 0));
 }
 
+#define MIN_HEIGHT_MONSTER_TO_WALK 1
+#define SMALLEST_HEIGHT_FOR_MONSTERS 1.4
+
+bool	can_traverse_monster(t_data *d, int i, t_vec2f *pos, t_sector *sect)
+{
+	int16_t neighbor;
+	double	cur_height;
+	t_wall	*wall;
+
+	wall = &d->walls[i];
+	cur_height = get_floorheight_point(d, sect - &d->sectors[0], *pos);
+	neighbor = wall->neighborsect;
+	return (wall->neighborsect != -1 &&
+			wall->is_transparent == false &&
+			d->doorstate[i] > 0.7 &&
+			cur_height + MIN_HEIGHT_MONSTER_TO_WALK > get_floorheight_point(d,
+			sect - &d->sectors[0], *pos) && (d->sectors[neighbor].outdoor ||
+			get_ceilheight_point(d, sect - &d->sectors[0], *pos) -
+			get_floorheight_point(d, sect - &d->sectors[0], *pos) >
+			SMALLEST_HEIGHT_FOR_MONSTERS));
+}
+
 bool	collision_monster_wall(t_data *d,
 		t_sector *sect, t_vec2f *pos, double dist_coll)
 {
@@ -96,8 +118,8 @@ bool	collision_monster_wall(t_data *d,
 				*pos);
 		v[1] = sub_vec2f(*pos, v[0]);
 		if ((dist = vec2f_length(v[1])) < dist_coll)
-			(can_traverse(d, i[0], &d->walls[i[0]])) ? (collided |=
-				collision_monster_wall(d,
+			(can_traverse_monster(d, i[0], pos, sect)) ?
+				(collided |= collision_monster_wall(d,
 				&d->sectors[d->walls[i[0]].neighborsect], pos, dist_coll)) :
 				((pos->x = v[0].x + v[1].x * dist_coll * 1.001 /
 				dist) && (pos->y = v[0].y + v[1].y * dist_coll *
